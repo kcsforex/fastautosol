6313312e1c6215a9ce98b2f3479ba9a469e7800e
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-05-14 13:02:21 +0200

    Update youtube_charts.py

--- pages/youtube_charts.py
+++ pages/youtube_charts.py
@@ -126,8 +126,7 @@ def make_card(title, content, is_graph=True, md_col=3):
         html.Div([
 
             html.H6(
-                title,
-                className="mb-2",
+                title, className="text-success mb-2,
                 style={
                     "color": "#ef4444",
                     "fontWeight": "500"
