1b2f492c7334ae9bbbea30e66471261af71e6ddf
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-05-14 21:39:05 +0200

    Update youtube_charts.py

--- pages/youtube_charts.py
+++ pages/youtube_charts.py
@@ -18,7 +18,7 @@ DB_CONFIG = "postgresql+psycopg://sql_admin:sql_pass@postgresql:5432/n8n"
 sql_engine = create_engine(DB_CONFIG, pool_size=5, max_overflow=10, pool_pre_ping=True, pool_recycle=1800,
     connect_args={'connect_timeout': 5, 'keepalives': 1, 'keepalives_idle': 30, 'keepalives_interval': 10, 'keepalives_count': 5})
 
-dash.register_page(__name__, icon="fa-brands fa-youtube", name="Youtube Dashboard",order=7)
+dash.register_page(__name__, icon="fa-youtube", name="Youtube Dashboard",order=7)
 
 # -------------------------------------------------
 # STYLE
