b867e9bfb5b107f78ea75f1f2e77d921a5c4c89b
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-05-15 10:14:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 2026.05.14  18.00
+# 2026.05.15  10.00
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
@@ -17,8 +17,7 @@ import apis.movies_api_dlt as movies_dlt
 import apis.youtube_api as youtube_api
 
 # ----- 1. Initalize Dash -----
-app = dash.Dash(__name__, use_pages=True, suppress_callback_exceptions=True, # compress=True
-    external_stylesheets=[dbc.themes.DARKLY, "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"])
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.DARKLY, "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"])
 
 # ----- 2. NOW IMPORT YOUR PAGES -----
 from pages import home, bybit_ccharts, bybit_lcharts, crm_serper, crm_shopify, mexc_charts, mcp, lufthansa_charts, youtube_charts
@@ -73,7 +72,7 @@ sidebar = html.Div([
     dbc.Nav([
         dbc.NavLink([
             html.Div([
-                html.I(className=f"fas {page.get('icon', 'fa-chart-line')} me-2"),
+                html.I(className=f"{page.get('icon', 'fa-solid fa-chart-line')} me-2")
                 html.Span(page["name"]),
             ], className="d-flex align-items-center")
         ], href=page["relative_path"], active="exact", className="mb-2 py-2 ps-2 rounded-3 text-light")
